5525aee9c3c1bc7efe810566eeaa039303c62e20
Wojciech Wroński <wwronski@acram.eu>
2023-08-26 11:31:28 +0200

    Initial Commit

---
+++ src/main/java/com/hyerodrimm/horsearmorstandmod/HorseArmorStandModClient.java
@@ -0,0 +1,10 @@
+package com.hyerodrimm.horsearmorstandmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HorseArmorStandModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/hyerodrimm/horsearmorstandmod/HorseArmorStandModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.hyerodrimm.horsearmorstandmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HorseArmorStandModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
